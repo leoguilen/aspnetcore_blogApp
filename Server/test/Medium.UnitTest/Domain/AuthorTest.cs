dbf5d2e38de3cc1716e45bb01771d170151a7a47
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-06-27 17:26:05 -0300

    Add test para a extensão do datetime

--- Server/test/Medium.UnitTest/Domain/AuthorTest.cs
+++ Server/test/Medium.UnitTest/Domain/AuthorTest.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Medium.Core.Common.Builder;
 using Medium.Core.Common.Extension;
+using Medium.Core.Domain;
 using System;
 using Xunit;
 
@@ -11,6 +12,43 @@ namespace Medium.UnitTest.Domain
     {
         [Fact]
         public void ShouldBeCreated_NewAuthor()
+        {
+            var faker = new Faker("pt_BR");
+            var expectedAuthor = new
+            {
+                Id = faker.Random.Guid(),
+                FirstName = faker.Person.FirstName,
+                LastName = faker.Person.LastName,
+                Username = faker.Person.UserName,
+                Password = faker.Internet.Password(),
+                Hash = faker.Random.AlphaNumeric(6),
+                Email = faker.Person.Email,
+                ConfirmedEmail = faker.Random.Bool(),
+                Deactivated = faker.Random.Bool(),
+                Deleted = faker.Random.Bool(),
+                CreatedAt = DateTime.Now.DefaultFormat(),
+                UpdatedAt = DateTime.Now.DefaultFormat()
+            };
+
+            var author = new Author
+            {
+                Id = expectedAuthor.Id,
+                FirstName = expectedAuthor.FirstName,
+                LastName = expectedAuthor.LastName,
+                Username = expectedAuthor.Username,
+                Password = expectedAuthor.Password,
+                Hash = expectedAuthor.Hash,
+                Email = expectedAuthor.Email,
+                ConfirmedEmail = expectedAuthor.ConfirmedEmail,
+                Deactivated = expectedAuthor.Deactivated,
+                Deleted = expectedAuthor.Deleted,
+            };
+
+            expectedAuthor.Should().BeEquivalentTo(author);
+        }
+
+        [Fact]
+        public void ShouldBeCreated_NewAuthor_UsingBuilder()
         {
             var faker = new Faker("pt_BR");
             var expectedAuthor = new
---
+++ Server/test/Medium.UnitTest/Extension/DateTimeExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Medium.Core.Common.Extension;
+using System;
+using Xunit;
+
+namespace Medium.UnitTest.Extension
+{
+    public class DateTimeExtensionsTest
+    {
+        [Fact]
+        public void ShouldBe_ReturnedDateTimeNow_WithoutSecondsValue()
+        {
+            var dateTimeNow = DateTime.Now;
+
+            var dateTimeFormated = dateTimeNow
+                .DefaultFormat();
+
+            dateTimeFormated.Should().HaveSecond(0);
+        }
+    }
+}
