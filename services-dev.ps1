0f7b1a639b2d0c7304972cac862b2514dbd0ab01
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-08-11 21:31:19 -0300

    Mudando configurações para ambiente de desenvolvimento, e script para subir serviços de teste

---
+++ services-dev.ps1
@@ -0,0 +1,4 @@
+# Subindo servidor de banco de dados SQL Server
+docker run --name medium-db -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Master2020#" -p 1433:1433 -v medium-db-sql:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+# Subindo serviço do SEQ
+docker run --rm -it -e ACCEPT_EULA=Y -p 5341:80 -d datalust/seq
