ff21a962e75f31a54f85b03af733cfe38eb5e16c
Leonardo Cruz Guilen <33328994+leoguilen@users.noreply.github.com>
2020-09-12 16:16:57 -0300

    Update services-dev.ps1

--- services-dev.ps1
+++ services-dev.ps1
@@ -1,6 +1,6 @@
 # Removendo containers parados
 docker container prune
 # Subindo servidor de banco de dados SQL Server
-docker run --name medium-db -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Master2020#" -p 1433:1433 -v medium-db-sql:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+docker run --name blog-db -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Master2020#" -p 1433:1433 -v medium-db-sql:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
 # Subindo serviço do SEQ
-docker run --rm -it -e ACCEPT_EULA=Y -p 5341:80 -d datalust/seq
+docker run --name blog-seq --rm -it -e ACCEPT_EULA=Y -p 5341:80 -d datalust/seq
